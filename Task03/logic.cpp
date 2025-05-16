65bcac0d6b7a9172e4bd5d084d4f40a892a0a2c5
ivanvikvik <ivanvikvik@gmail.com>
2025-05-16 19:34:35 +0300

    Solve Task03

---
+++ Task03/logic.cpp
@@ -0,0 +1,13 @@
+int sum_all_elements(int* array, int size) {
+	if (array == nullptr || size < 0) {
+		return 0;
+	}
+
+	// base case
+	if (size == 1) {
+		return array[0];
+	}
+
+	// recursion case
+	return array[size - 1] + sum_all_elements(array, size - 1);
+}
